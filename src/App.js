f705c8d460eaaae3fa8be85c95e0595cf25aaa27
Gabriella Morgan <gabriellapelton@gmail.com>
2022-12-04 08:51:56 -0600

    redirect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  window.location.replace("https://app.inputs.ag");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Redirecting...</header>
     </div>
   );
 }
